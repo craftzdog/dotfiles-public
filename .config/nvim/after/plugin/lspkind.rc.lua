7ee82fba1c5fa63a5e2a1492b283139aab2947be
Takuya Matsuyama <hi@craftz.dog>
2022-02-08 11:38:42 +0900

    fix(nvim): update lspkind config
    with_text is deprecated
    
    fix #32

--- .config/nvim/after/plugin/lspkind.rc.lua
+++ .config/nvim/after/plugin/lspkind.rc.lua
@@ -5,7 +5,7 @@ lspkind.init({
   -- enables text annotations
   --
   -- default: true
-  with_text = true,
+  mode = 'symbol',
 
   -- default symbol map
   -- can be either 'default' (requires nerd-fonts font) or
