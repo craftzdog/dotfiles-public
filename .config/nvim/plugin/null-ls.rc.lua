2ef5401b727136beeb051292061c8dba1134f935
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2022-08-10 21:21:46 -0300

    feat(neovim): improve config null-ls

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -1,11 +1,19 @@
 local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
-null_ls.setup({
+null_ls.setup {
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_command [[augroup Format]]
+      vim.api.nvim_command [[autocmd! * <buffer>]]
+      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+      vim.api.nvim_command [[augroup End]]
+    end
+  end,
   sources = {
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
     null_ls.builtins.diagnostics.fish
   }
-})
+}
