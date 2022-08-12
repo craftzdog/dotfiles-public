14b8ffa7a068569047cdbd64e1c50dffa12ea927
Zheng Ma <mazhengcn@outlook.com>
2022-08-12 12:31:22 +0800

    fix(fish): change macOS and peco configs

--- .config/fish/config-osx.fish
+++ .config/fish/config-osx.fish
@@ -2,3 +2,5 @@ if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
 end
+
+set -gx PATH /opt/homebrew/bin $PATH
--- .config/fish/functions/peco_change_directory.fish
+++ .config/fish/functions/peco_change_directory.fish
@@ -18,6 +18,6 @@ function peco_change_directory
     echo $HOME/.config
     ghq list -p
     ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
-    ls -ad $HOME/Developments/*/* |grep -v \.git
+    #    ls -ad $HOME/repo/*/* |grep -v \.git
   end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
 end
