d337f7551c82bcedf0e910a6c35ccb9197ead967
Microctar Lecter <Microctar@outlook.com>
2022-08-15 11:33:24 +0800

    chore(nvim): migrate bufferline config migrate akinsho/bufferline.nvim config to v2.6.0 see akinsho/bufferline.nvim#499 for more details

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -12,22 +12,22 @@ bufferline.setup({
   },
   highlights = {
     separator = {
-      guifg = '#073642',
-      guibg = '#002b36',
+      fg = '#073642',
+      bg = '#002b36',
     },
     separator_selected = {
-      guifg = '#073642',
+      fg = '#073642',
     },
     background = {
-      guifg = '#657b83',
-      guibg = '#002b36'
+      fg = '#657b83',
+      bg = '#002b36'
     },
     buffer_selected = {
-      guifg = '#fdf6e3',
-      gui = "bold",
+      fg = '#fdf6e3',
+      bold = true,
     },
     fill = {
-      guibg = '#073642'
+      bg = '#073642'
     }
   },
 })
