34a120bd071a051bd91f2b15ab01ce1cb8aeff3c
Zheng Ma <mazhengcn@outlook.com>
2022-09-25 14:05:23 +0800

    fix(powershell): correct path of ohmyposh theme file

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -2,8 +2,7 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/takuya.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
