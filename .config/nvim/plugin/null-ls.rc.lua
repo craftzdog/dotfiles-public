940ed444f911fb2a3c1592734de16721095b7510
Takuya Matsuyama <hi@craftz.dog>
2022-10-10 13:37:13 +0900

    fix(nvim): update format-on-save config
    now it requires Neovim >= 0.8
    
    fix #75

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -1,23 +1,43 @@
 local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
 
 null_ls.setup {
   sources = {
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
     null_ls.builtins.diagnostics.fish
   },
   on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup_format,
-        buffer = 0,
-        callback = function() vim.lsp.buf.formatting_seq_sync() end
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
       })
     end
-  end,
+  end
 }
+
+vim.api.nvim_create_user_command(
+  'DisableLspFormatting',
+  function()
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+  end,
+  { nargs = 0 }
+)
