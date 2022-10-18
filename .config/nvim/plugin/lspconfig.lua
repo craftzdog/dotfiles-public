f6473accf6f647cd847650f42fdd07af12cd2762
Shan-A-Rownok Bosunia <rownokbosunia.dev@gmail.com>
2022-10-18 12:07:44 +0600

    cmp_nvim_lsp.update_capabilities is deprecated

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -65,7 +65,7 @@ protocol.CompletionItemKind = {
 }
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
