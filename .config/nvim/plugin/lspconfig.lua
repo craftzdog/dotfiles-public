f845da0ce09fccbce4877f80a72b2e24bd504706
Takuya Matsuyama <hi@craftz.dog>
2022-10-20 21:21:51 +0900

    fix(nvim): unnecessary argument
    https://github.com/hrsh7th/cmp-nvim-lsp/issues/38
    
    fix #79

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -65,9 +65,7 @@ protocol.CompletionItemKind = {
 }
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').default_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 nvim_lsp.flow.setup {
   on_attach = on_attach,
