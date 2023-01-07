1ced12032cdaa039ac8ef60eea632df3a8786a34
Leon <s8952889@gmail.com>
2023-01-07 19:12:30 +0800

    feat(nvim): enhance nvim-cmp for tailwindcss

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -23,7 +23,26 @@ cmp.setup({
     { name = 'buffer' },
   }),
   formatting = {
-    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+    format = lspkind.cmp_format({
+      maxwidth = 50,
+      before = function(entry, vim_item) -- for tailwind css autocomplete
+        if vim_item.kind == 'Color' and entry.completion_item.documentation then
+          local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+          if r then
+            local color = string.format('%02x', r) .. string.format('%02x', g) ..string.format('%02x', b)
+            local group = 'Tw_' .. color
+            if vim.fn.hlID(group) < 1 then
+              vim.api.nvim_set_hl(0, group, {fg = '#' .. color})
+            end
+            vim_item.kind = "⬤"
+            vim_item.kind_hl_group = group
+            return vim_item
+          end
+        end
+        vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+        return vim_item
+      end
+    })
   }
 })
 
