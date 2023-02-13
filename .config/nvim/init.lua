0c7f78eda8871ed3df54c6de29a67b89684310c2
BrunchTea <ScreeperC@outlook.com>
2023-02-13 14:07:29 +0800

    Connect the clipboard between Windows and WSL2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,10 +6,14 @@ require('craftzdog.plugins')
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
+local is_wsl = has "wsl"
 
 if is_mac then
   require('craftzdog.macos')
 end
 if is_win then
   require('craftzdog.windows')
+end
+if is_wsl then
+  require('craftzdog.wsl')
 end
