f8d4835368ac26328d9861ae6ed5b008c4968fc4
harshv1741 <harshv1741@gmail.com>
2023-02-14 10:12:05 +0530

    fix(sumneko_lua)
    Replacing sumneko_lua with lua_ls

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -84,7 +84,7 @@ nvim_lsp.sourcekit.setup {
   capabilities = capabilities,
 }
 
-nvim_lsp.sumneko_lua.setup {
+nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
