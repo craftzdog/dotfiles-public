943257f4238092e3d3e2f996ed0215fc8242d8de
CofCat456 <a25690756@gmail.com>
2023-08-01 15:04:58 +0800

    fix(nvim): Switch from glepnir/lspsaga.nvim to nvimdev/lspsaga.nvim

--- .config/nvim/lua/craftzdog/plugins.lua
+++ .config/nvim/lua/craftzdog/plugins.lua
@@ -23,7 +23,7 @@ packer.startup(function(use)
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
 
-  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'nvimdev/lspsaga.nvim' -- LSP UIs
   use 'L3MON4D3/LuaSnip'
   use {
     'nvim-treesitter/nvim-treesitter',
