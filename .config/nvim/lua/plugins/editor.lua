19945ddf23030658dc7461a67fd093d891a2dd7c
Takuya Matsuyama <hi@craftz.dog>
2024-01-10 09:35:02 +0900

    fix(nvim): add some type annotations for hsl/rgb conversions

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -22,8 +22,11 @@ return {
 					pattern = "hsl%(%d+,? %d+,? %d+%)",
 					group = function(_, match)
 						local utils = require("solarized-osaka.hsl")
-						local h, s, l = match:match("hsl%((%d+),? (%d+),? (%d+)%)")
-						h, s, l = tonumber(h), tonumber(s), tonumber(l)
+						--- @type string, string, string
+						local nh, ns, nl = match:match("hsl%((%d+),? (%d+),? (%d+)%)")
+						--- @type number?, number?, number?
+						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+						--- @type string
 						local hex_color = utils.hslToHex(h, s, l)
 						return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
 					end,
