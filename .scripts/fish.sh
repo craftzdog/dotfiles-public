e75ce3b1d5f380e9bfec052bde51609a7542c331
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-16 11:44:58 +0530

    feat(nvim): Restoring my `#146` deleted commit by mistake

---
+++ .scripts/fish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+set CONFIG_PATH ~/.config/fish
+set BASE_DIR $HOME/.dotfiles/.config/fish
+ln -s "$BASE_DIR/config.fish" "$CONFIG_PATH"
+ln -s "$BASE_DIR/config-osx.fish" "$CONFIG_PATH"
+ln -s "$BASE_DIR/config-windows.fish" "$CONFIG_PATH"
+ln -s "$BASE_DIR/config-linux.fish" "$CONFIG_PATH"
+ln -s "$BASE_DIR/functions/fzf_change_directory.fish" "$CONFIG_PATH/functions/"
+ln -s "$BASE_DIR/functions/fish_user_key_bindings.fish" "$CONFIG_PATH/functions/"
+ln -s "$BASE_DIR/conf.d/tide.fish" "$CONFIG_PATH/conf.d/"
+ln -s "$BASE_DIR/fish_plugins" "$CONFIG_PATH"
