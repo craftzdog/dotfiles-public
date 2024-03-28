382313b02a7da3a4e99f603c625797cfc93ca16b
Harsh Vyapari (tumbleweed WSL) <harshv5094@gmail.com>
2024-03-28 13:37:15 +0530

    fix(fish): Removing if condition as it is creating problems

--- .scripts/fish.sh
+++ .scripts/fish.sh
@@ -21,13 +21,10 @@ ln -s "$BASE_DIR/functions/fzf_change_directory.fish" "$CONFIG_PATH/functions/"
 ln -s "$BASE_DIR/functions/fish_user_key_bindings.fish" "$CONFIG_PATH/functions/"
 ln -s "$BASE_DIR/conf.d/tide.fish" "$CONFIG_PATH/conf.d/"
 
-# Check if fisher is installed
-if not functions -q fisher
-    echo "fisher is not installed. Installing fisher..."
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-    echo "fisher installed successfully."
-    cp -rf fish_plugins "$CONFIG_PATH"
-    fish -c "fisher update"
-end
+echo "Installing fisher..."
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+echo "fisher installed successfully."
+cp -rf fish_plugins "$CONFIG_PATH"
+fish -c "fisher update"
 
 
