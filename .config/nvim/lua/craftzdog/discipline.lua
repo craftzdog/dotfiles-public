ca8664049edcd90c22849bbb30710bf43a2de881
tony-sn <57897153+tony-sn@users.noreply.github.com>
2024-12-12 00:50:32 +1100

    refactor: reduce to single notified toast.

--- .config/nvim/lua/craftzdog/discipline.lua
+++ .config/nvim/lua/craftzdog/discipline.lua
@@ -2,26 +2,24 @@ local M = {}
 
 function M.cowboy()
 	---@type table?
-	local id
 	local ok = true
 	for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
 		local count = 0
-		local timer = assert(vim.loop.new_timer())
+		local timer = assert(vim.uv.new_timer())
 		local map = key
 		vim.keymap.set("n", key, function()
 			if vim.v.count > 0 then
 				count = 0
 			end
-			if count >= 10 then
-				ok, id = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
+			if count >= 10 and vim.bo.buftype ~= "nofile" then
+        			ok = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
 					icon = "🤠",
-					replace = id,
+					id = "cowboy",
 					keep = function()
 						return count >= 10
 					end,
 				})
 				if not ok then
-					id = nil
 					return map
 				end
 			else
