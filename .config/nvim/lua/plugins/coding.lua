d945589194c6c2a25ef6f42034023974cfce1082
Takuya Matsuyama <hi@craftz.dog>
2025-02-27 09:52:39 +0900

    fix(nvim): remove unused plugins

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,42 +1,9 @@
 return {
-	-- Create annotations with one keybind, and jump your cursor in the inserted annotation
-	{
-		"danymat/neogen",
-		keys = {
-			{
-				"<leader>cc",
-				function()
-					require("neogen").generate({})
-				end,
-				desc = "Neogen Comment",
-			},
-		},
-		opts = { snippet_engine = "luasnip" },
-	},
-
 	-- Incremental rename
 	{
 		"smjonas/inc-rename.nvim",
 		cmd = "IncRename",
 		config = true,
-	},
-
-	-- Refactoring tool
-	{
-		"ThePrimeagen/refactoring.nvim",
-		keys = {
-			{
-				"<leader>r",
-				function()
-					require("refactoring").select_refactor()
-				end,
-				mode = "v",
-				noremap = true,
-				silent = true,
-				expr = false,
-			},
-		},
-		opts = {},
 	},
 
 	-- Go forward/backward with square brackets
@@ -85,13 +52,5 @@ return {
 		opts = {
 			position = "right",
 		},
-	},
-
-	{
-		"nvim-cmp",
-		dependencies = { "hrsh7th/cmp-emoji" },
-		opts = function(_, opts)
-			table.insert(opts.sources, { name = "emoji" })
-		end,
 	},
 }
