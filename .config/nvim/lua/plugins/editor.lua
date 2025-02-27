e33adf716945af64a70b93fc8130c36fb85022af
Takuya Matsuyama <hi@craftz.dog>
2025-02-27 09:52:48 +0900

    feat(nvim): add close buffers

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -212,6 +212,27 @@ return {
 	},
 
 	{
+		"kazhala/close-buffers.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>th",
+				function()
+					require("close_buffers").delete({ type = "hidden" })
+				end,
+				"Close Hidden Buffers",
+			},
+			{
+				"<leader>tu",
+				function()
+					require("close_buffers").delete({ type = "nameless" })
+				end,
+				"Close Nameless Buffers",
+			},
+		},
+	},
+
+	{
 		"saghen/blink.cmp",
 		opts = {
 			completion = {
